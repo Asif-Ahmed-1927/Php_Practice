ad4311395aab91299405f54e0d54bc210c916646
Asif Ahmed <asifbin8@gmail.com>
2022-04-09 23:55:18 +0600

    commit

---
+++ coockies.php
@@ -0,0 +1,14 @@
+<?php require "includes/link_style.php"; ?>
+
+<?php
+
+session_start();
+
+echo $_SESSION['user'];
+
+?>
+
+
+
+<?php require 'includes/footer.php';?>
+
--- test.php
+++ test.php
@@ -42,24 +42,50 @@
 
 </form>-->
 
-<!-- <?php
+ <?php
 
 //if(isset($_REQUEST['wrongPass'])){
     //echo $_REQUEST['wrongPass'];
 
 //}
-?> -->
+
+
+?> 
 
 <!-- PASS ENCRYPTED -->
 
-<form action="login.php" method="get">
+<!-- <form action="login.php" method="get">
 
     <input type="text" name="username" placeholder="username"><br><br>    
     <input type="email" name="email" placeholder="email"><br><br>    
     <input type="password" name="password" placeholder="password"><br><br>    
     <input type="submit" value="submit">
 
-</form>
+</form> -->
+
+<!-- cookie -->
+
+<?php
+
+// $name= "user";
+// $value="Asif";
+
+// setcookie($name, $value, time()+20);
+
+// if (isset($_COOKIE['user'])){
+//     echo "saved cookie is= {$_COOKIE['user']}";
+// }else{
+//     echo "Cookie is not set";
+// }
+
+//session
+
+session_start();
+
+$_SESSION['user']="Asif"
+
+?>
+
 
 
 
